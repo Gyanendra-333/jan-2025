939f555c0a4e601dc78bf41944aa80b768e9b6ce
Gyanendra <pgyanendra333@gmail.com>
2025-01-11 15:51:34 +0530

    create short url

--- 03/short-url/controllers/url.controller.js
+++ 03/short-url/controllers/url.controller.js
@@ -1,10 +1,10 @@
-const { nanoid } = require("nanoid");
+const shortid = require("shortid");
 const URL = require("../models/user.model.js");
 
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
     if (!body.URL) return res.status(400).json({ error: "URL is required" })
-    const shortID = nanoid(8);
+    const shortID = shortid(8);
     await URL.create({
         shortId: shortID,
         redirectURL: shortID,
@@ -12,3 +12,5 @@ async function handleGenerateNewShortURL(req, res) {
     })
     return res.json({ id: shortID });
 }
+
+module.exports = { handleGenerateNewShortURL };
--- 03/short-url/index.js
+++ 03/short-url/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const urlRoute = require("./routes/url.route.js");
+const { connectDB } = require("./db.js");
 
 const app = express();
 const PORT = 4001;
+
+// Database 
+connectDB("mongodb+srv://frontend4682:blog123@cluster0.ee9cg.mongodb.net/")
+    .then(console.log("Database Connected Successfully "))
+
+// middleware 
+app.use(express.json());
+
+// Route 
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running port : ${PORT}`);
