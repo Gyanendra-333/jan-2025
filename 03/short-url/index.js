5b855c1b3303d313230f932148899e7d908ed6eb
Gyanendra <pgyanendra333@gmail.com>
2025-01-13 17:03:56 +0530

    basic change

--- 03/short-url/index.js
+++ 03/short-url/index.js
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is Running Port : ${PORT}`);
+    console.log(`Server is Running Port ${PORT}`);
 })
