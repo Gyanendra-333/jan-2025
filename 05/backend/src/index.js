354251e14b4df5d762b128c0dde222fc692f694c
Gyanendra <pgyanendra333@gmail.com>
2025-01-15 18:04:22 +0530

    create utils

--- 05/backend/src/index.js
+++ 05/backend/src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
-
-
-
-connectDB();
+// Database Connection 
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is Running Port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Error !", err);
+    })
