1027e00cfbf3b37c2c8e091c9e2517c5effdfd4b
Gyanendra <pgyanendra333@gmail.com>
2025-01-15 15:53:41 +0530

    basic changes

--- 05/backend/src/db/index.js
+++ 05/backend/src/db/index.js
@@ -7,6 +7,8 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log("MongoDB Connected Successfully");
 
+        // console.log(`MongoDB Connected Successfully !! DB Host : ${connectionInstance.connection.host}`);
+
     } catch (error) {
         console.log("mongoDB Connection error", error);
         process.exit(1);
