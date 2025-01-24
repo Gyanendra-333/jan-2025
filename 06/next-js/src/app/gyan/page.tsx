c6eeac1eabf3792f46571851c2f21df80b708e09
Gyanendra <pgyanendra333@gmail.com>
2025-01-24 13:13:57 +0530

    basic structure setup

---
+++ 06/next-js/src/app/gyan/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function gyanPage(){
+    return(
+         <div className="font-bold text-center text-3xl bg-sky-200 border-black border">
+    <h1>Gyan Page</h1>
+   </div>
+    )
+}
