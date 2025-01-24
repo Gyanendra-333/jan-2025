d0bbd271c5881b5f14fae41d0c22e1083f0f4d1d
Gyanendra <pgyanendra333@gmail.com>
2025-01-24 14:49:49 +0530

    add folders and json

--- 06/next-js/src/app/gyan/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function gyanPage(){
-    return(
-         <div className="font-bold text-center text-3xl bg-sky-200 border-black border">
-    <h1>Gyan Page</h1>
-   </div>
-    )
-}
