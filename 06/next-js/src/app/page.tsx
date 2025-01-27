a32d1ce9181296b85c11a96b338ecd1beb53a957
Gyanendra <pgyanendra333@gmail.com>
2025-01-27 17:27:59 +0530

    add card

--- 06/next-js/src/app/page.tsx
+++ 06/next-js/src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[.90] antialiased bg-grid-white/[0.02] text-white">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
