22a39b2a5f758797c5b812564916cec94dae8e35
Gyanendra <pgyanendra333@gmail.com>
2025-02-10 12:02:02 +0530

    create react project using tailwind

---
+++ 07/redux/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-800'>Hello Redux</h1>
+
+    </>
+  )
+}
+
+export default App
