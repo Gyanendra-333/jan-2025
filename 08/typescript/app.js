942b943f5ada22c05267e318e97fe333f2ab6a4f
Gyanendra <pgyanendra333@gmail.com>
2025-02-26 18:52:26 +0530

    typescript

---
+++ 08/typescript/app.js
@@ -0,0 +1,6 @@
+var num = 33;
+var str = "Gyan";
+var isTrrue = false;
+console.log(num);
+console.log(str);
+console.log(isTrrue);
---
+++ 08/typescript/app.ts
@@ -0,0 +1,8 @@
+
+let num: number = 33;
+var str: string = "Gyan";
+var isTrrue: boolean = false
+
+console.log(num);
+console.log(str);
+console.log(isTrrue);
