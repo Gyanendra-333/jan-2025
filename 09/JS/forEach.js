91f0fa89500a046be3d2775fc9e3b84680f2fbfc
Gyanendra <pgyanendra333@gmail.com>
2025-02-28 18:58:08 +0530

    dom Revision

---
+++ 09/JS/forEach.js
@@ -0,0 +1,12 @@
+
+
+let arr = ["Gyan", "Shyam", "Name"];
+
+arr.forEach((item) => {
+    console.log(item)
+});
+
+let num = [1, 2, 3, 5, 6, 7]
+
+let newArr = num.map((item) => item + 10)
+console.log(newArr);
